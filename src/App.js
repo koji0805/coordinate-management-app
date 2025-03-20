e94e11c19d1162ca4f3a946b2fac02b3651138d2
Kie Sato <kie.1993@icloud.com>
2025-03-20 10:29:12 +0900

    add: 初期ファイルの追加

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    // http://localhost:8000/api/todoにアクセスし、todoのリストを取得
+    fetch("http://localhost:8000/api/todo")
+      .then((response) => response.json())
+      .then((data) => setTodos(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      {/* todosの内容をリスト(<ul><li>)を使用して一覧表示 */}
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
