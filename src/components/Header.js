97a7bb02b09f5ad4e51bdb60b1425c7a287d8ec8
Kie Sato <kie.1993@icloud.com>
2025-03-26 05:32:54 +0900

    add: ゲスト用ヘッダーナビゲーション

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,23 @@ const Header = ({ isLoggedIn, onLogout }) => {
 
     const ItemsForUser = () => {
         return (<>
-            <div className="float-right text-base">
-                <CustomNavLink to="/home">ホーム</ CustomNavLink>
-                <CustomNavLink to="/mypage">マイページ</ CustomNavLink>
-                <button className="bg-slate-50 text-sky-600 p-[4px] rounded-md hover:bg-sky-100 cursor-pointer" onClick={onLogout}>ログアウト</button>
-            </div>
+            <CustomNavLink to="/home">ホーム</ CustomNavLink>
+            <CustomNavLink to="/mypage">マイページ</ CustomNavLink>
+            <button className="bg-slate-50 text-sky-600 p-[4px] rounded-md hover:bg-sky-100 cursor-pointer" onClick={onLogout}>ログアウト</button>
+        </>)
+    }
+
+    const ItemsForGuest = () => {
+        return (<>
+            <CustomNavLink to="/signup">新規登録</ CustomNavLink>
+            <CustomNavLink to="/login">ログイン</ CustomNavLink>
         </>)
     }
     return (<>
         <header className="bg-sky-600 text-slate-50 p-[20px] text-xl ">
-            {isLoggedIn && <ItemsForUser />}
+            <div className="float-right text-base">
+                {isLoggedIn ? <ItemsForUser /> : <ItemsForGuest />}
+            </div>
             <h1>着物メモ</h1>
         </header>
     </>);
