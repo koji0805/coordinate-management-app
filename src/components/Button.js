9fd699cdaf2f4875c99683be8c5362f1240e8195
Kie Sato <kie.1993@icloud.com>
2025-03-26 05:16:01 +0900

    fix: ボタンホバー時は透過

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 const Button = ({ children }) => {
     return (
-        <button className="w-[100%] py-[8px] bg-sky-600 hover:bg-sky-700 text-slate-50 rounded-md cursor-pointer mb-[16px]">{children}</button>
+        <button className="w-[100%] py-[8px] bg-sky-600 hover:opacity-50 text-slate-50 rounded-md cursor-pointer mb-[16px]">{children}</button>
     )
 }
 
