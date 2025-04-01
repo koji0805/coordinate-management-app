d1bd09e72f26297fefb93bc8c11fe799a288c930
Kie Sato <kie.1993@icloud.com>
2025-04-01 09:09:15 +0900

    change: リフレッシュトークンが使用できるよう修正

--- src/api/authAPI.js
+++ src/api/authAPI.js
@@ -14,10 +14,28 @@ export const signup = async (data) => {
 }
 
 export const authMe = async (access_token) => {
-    const response = await apiClient.get('/auth/me', {
-        headers: {
-            Authorization: `Bearer ${access_token}`
-        }
-    });
-    return response.data
+    try {
+        const response = await apiClient.get('/auth/me', {
+            headers: {
+                Authorization: `Bearer ${access_token}`
+            }
+        });
+        return response.data
+    } catch (error) {
+        const refresh_token = localStorage.getItem('refresh_token');
+        const refresh_response = await apiClient.post('/auth/refresh', null, {
+            headers: {
+                Authorization: `Bearer ${refresh_token}`
+            }
+        })
+        const newAccessToken = refresh_response.data.access_token;
+        const retry_response = await apiClient.get('/auth/me', {
+            headers: {
+                Authorization: `Bearer ${newAccessToken}`
+            }
+        });
+        localStorage.setItem('access_token', newAccessToken);
+        return retry_response.data
+    }
+
 }
